161b7f2dca28fa1b8b0900d173a7a06ff50a671e
shafik-2203 <shafik2203@gmail.com>
2025-07-09 19:43:09 +0530

    a

--- src/pages/OTP.tsx
+++ src/pages/OTP.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../contexts/AuthContext";
 import { apiClient } from "../lib/api";
 import Logo from "../components/Logo";
 import WelcomeAnimation from "../components/WelcomeAnimation";
