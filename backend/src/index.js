b1070d2ee1d0f780c3fc0f9db4b101b8236a2c03
shafik-2203 <shafik2203@gmail.com>
2025-07-10 12:33:37 +0530

    a

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,8 +31,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      process.env.FRONTEND_URL || "http://localhost:5173",
-      "https://fullstack-pj1-fd.netlify.app",
+      process.env.FRONTEND_URL, "http://localhost:5173",
     ],
     credentials: true,
   })
